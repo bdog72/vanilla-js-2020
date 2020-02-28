e8506e7990d75f39efc2abb488304b45466e85a9
Brian Bycynski <high5bri@hotmail.com>
2020-02-28 03:18:42 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 //
+//
+console.log('Bozo Boy');
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log('Hello Bozo', i);
+// }
+
+// for (let num = 1; num <= 20; num++) {
+//   console.log(`${num}x${num} = ${num * num}`);
+// }
+
+// for (let i = 200; i >= 0; i -= 25) {
+//   console.log(i);
+// }
