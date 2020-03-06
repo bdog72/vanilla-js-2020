c1ac04a3ac8d3b429352aade1d38c4857a0c3646
Brian Bycynski <high5bri@hotmail.com>
2020-03-06 03:05:53 -0500

    starting with canvas API

--- projects/17-breakout/script.js
+++ projects/17-breakout/script.js
@@ -6,6 +6,12 @@ const rulesBtn = document.getElementById('rules-btn');
 const closeBtn = document.getElementById('close-btn');
 const rules = document.getElementById('rules');
 
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'red';
+ctx.fillRect(10, 10, 150, 100);
+
 rulesBtn.addEventListener('click', () => {
   rules.classList.add('show');
 });
